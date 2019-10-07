4186750293a4be754726b62174866095e32e7dbd
gdhaison <vuhaisonhb456@gmail.som>
2019-10-07 15:47:33 +0700

    Chapter 12: Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   USER_PARAMS = %i(name email password password_confirmation).freeze
+  PASSWORD_RESET_PARAMS = %i(password password_confirmation).freeze
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_digest
   before_save :downcase_email
 
@@ -49,11 +50,25 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_attributes activated: true, activated_at: Time.zone.now
+    update_columns activated: true, activated_at: Time.zone.now
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token), reset_sent_at:
+      Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.expire.hours.ago
   end
 
   private
